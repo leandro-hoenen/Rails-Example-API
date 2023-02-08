b3f5263f817c15e7d595f4134ba1575b852aa1ab
Leandro Hoenen <leandrohoenen@MBP-von-Leandro.fritz.box>
2023-02-08 16:31:02 +0100

    removed comments

--- app/services/consumer_service.rb
+++ app/services/consumer_service.rb
@@ -5,16 +5,11 @@ class ConsumerService
       @new_connection = BaseService.new
       @new_connection.start
       @channel = @new_connection.create_channel
-      # This tells RabbitMQ not to give more than one message to a worker at a time. 
-      # Or, in other words, don't dispatch a new message to a worker 
-      # until it has processed and acknowledged the previous one
        
       @channel.prefetch(1)
       puts "Consumeer Channel Created"
     end
   
-    # Declaring a queue is idempotent - it will only be created if it doesn't exist already
-    # durable: true make sures that queue will survive a RabbitMQ node restart
     def declare_queue(queue_name = "default", durable = false)
       puts "A queue with name #{queue_name} declared"
       @channel.queue(queue_name, durable: durable)
@@ -23,16 +18,11 @@ class ConsumerService
     def consume_message(queue)
       puts "Start Consuming Message"
       begin
-        # block: true is only used to keep the main thread
-        # alive. Please avoid using it in real world applications.
-        # manual_ack: Just to get manual acknowledgment
-        # If ack not received RabbitMQ will understand that a message wasn't processed fully and will re-queue it
         msg = nil
         queue.subscribe(manual_ack: true, block: true) do |delivery_info, _properties, payload|
           puts " [x] Received '#{payload}'"
           channel.ack(delivery_info.delivery_tag)
           msg = payload
-          #close_connection
         end
         return msg
       rescue Interrupt => _
--- app/services/publisher_service.rb
+++ app/services/publisher_service.rb
@@ -8,15 +8,11 @@ class PublisherService
       puts "Publisher Channel Created"
     end
   
-    #Declaring a queue is idempotent - it will only be created if it doesn't exist already
-    # durable: true make sures that queue will survive a RabbitMQ node restart
-    #Exclusive (used by only one connection and the queue will be deleted when that connection closes)
     def declare_queue(queue_name = "default", durable = true)
       puts "A queue with name #{queue_name} declared"
       @channel.queue(queue_name, durable: durable)
     end
   
-    # persistent: true it tells RabbitMQ to save the message to disk
     def publish_message(routing_key, msg, persistent = false, correlation_id = "a_secret_key")
       puts " Message sent #{msg}"
       @channel.default_exchange.publish(msg, routing_key: routing_key, persistent: persistent, correlation_id: correlation_id )
